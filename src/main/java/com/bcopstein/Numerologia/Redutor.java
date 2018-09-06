418ddd4b612ae290cde19012f99d34f44f49783f
PRISCILLA DA SILVA DEUS <17111267@pucrs.br>
2018-09-06 12:53:12 -0300

    redutor att, teste parcial

--- src/main/java/com/bcopstein/Numerologia/Redutor.java
+++ src/main/java/com/bcopstein/Numerologia/Redutor.java
@@ -11,156 +11,148 @@ import java.util.regex.Pattern;
 // Qualquer quebra destas regras deve retornar "IllegalArgumentExpression"
 //
 // Veja os tipos de tabelas na wiki do projeto
-public class Redutor{
-    private TipoTabela tipoTab;
+public class Redutor {
+	private TipoTabela tipoTab;
 
-    public Redutor(){
-        tipoTab = TipoTabela.PITAGORICA;
-    }
+	public Redutor() {
+		tipoTab = TipoTabela.PITAGORICA;
+	}
 
-    // Decodifica um caracter segundo a tabela Pitagorica
-    private int tabPitagorica(char c){
-        switch(c){
-            case 'A':
-            case 'J':
-            case 'S':
-                return(1);
-            case 'B':
-            case 'K':
-            case 'T':
-                return(2);
-            case 'C':
-            case 'L':
-            case 'U':
-                return(3);
-            case 'D':
-            case 'M':
-            case 'V':
-                return(4);
-            case 'E':
-            case 'N':
-            case 'W':
-                return(5);
-            case 'F':
-            case 'O':
-            case 'X':
-                return(6);
-            case 'G':
-            case 'P':
-            case 'Y':
-                return(7);
-            case 'H':
-            case 'Q':
-            case 'Z':
-                return(8);
-            case 'I':
-            case 'R':
-                return(9);
-            default:
-                throw new IllegalArgumentException("Caracter: "+c);
-        }
-    }
+	// Decodifica um caracter segundo a tabela Pitagorica
+	private int tabPitagorica(char c) {
+		switch (c) {
+		case 'A':
+		case 'J':
+		case 'S':
+			return (1);
+		case 'B':
+		case 'K':
+		case 'T':
+			return (2);
+		case 'C':
+		case 'L':
+		case 'U':
+			return (3);
+		case 'D':
+		case 'M':
+		case 'V':
+			return (4);
+		case 'E':
+		case 'N':
+		case 'W':
+			return (5);
+		case 'F':
+		case 'O':
+		case 'X':
+			return (6);
+		case 'G':
+		case 'P':
+		case 'Y':
+			return (7);
+		case 'H':
+		case 'Q':
+		case 'Z':
+			return (8);
+		case 'I':
+		case 'R':
+			return (9);
+		default:
+			throw new IllegalArgumentException("Caracter: " + c);
+		}
+	}
 
-    // Decodifica um caracter segundo a tabela Chaldean
-    private int tabChaldean(char c){
-        switch(c){
-            case 'A':
-            case 'I':
-            case 'J':
-            case 'Q':
-            case 'Y':
-                return(1);
-            case 'B':
-            case 'K':
-            case 'R':
-                return(2);
-            case 'C':
-            case 'G':
-            case 'L':
-            case 'S':
-                return(3);
-            case 'D':
-            case 'M':
-            case 'T':
-                return(4);
-            case 'E':
-            case 'H':
-            case 'N':
-            case 'X':
-                return(5);
-            case 'U':
-            case 'V':
-            case 'W':
-                return(6);
-            case 'O':
-            case 'Z':
-                return(7);
-            case 'F':
-            case 'P':
-                return(8);
-            default:
-                throw new IllegalArgumentException("Caracter: "+c);
-        }
-    }
+	// Decodifica um caracter segundo a tabela Chaldean
+	private int tabChaldean(char c) {
+		switch (c) {
+		case 'A':
+		case 'I':
+		case 'J':
+		case 'Q':
+		case 'Y':
+			return (1);
+		case 'B':
+		case 'K':
+		case 'R':
+			return (2);
+		case 'C':
+		case 'G':
+		case 'L':
+		case 'S':
+			return (3);
+		case 'D':
+		case 'M':
+		case 'T':
+			return (4);
+		case 'E':
+		case 'H':
+		case 'N':
+		case 'X':
+			return (5);
+		case 'U':
+		case 'V':
+		case 'W':
+			return (6);
+		case 'O':
+		case 'Z':
+			return (7);
+		case 'F':
+		case 'P':
+			return (8);
+		default:
+			throw new IllegalArgumentException("Caracter: " + c);
+		}
+	}
 
-    // Decodifica um caracter segundo a tabela selecionada
-    private int decodChar(char c){
-        if(tipoTab.equals(TipoTabela.PITAGORICA)) {
-        	return tabPitagorica(c);
-        } else {
-        	return tabChaldean(c);
-        }
-    }
+	// Decodifica um caracter segundo a tabela selecionada
+	private int decodChar(char c) {
+		if (tipoTab.equals(TipoTabela.PITAGORICA)) {
+			return tabPitagorica(c);
+		} else {
+			return tabChaldean(c);
+		}
+	}
 
-    private String reducaoSimples(String str){
-        //TODO
-        return null;
-    }
+	/*
+	 * private String reducaoSimples(String str) { // TODO return null; }
+	 */
 
-    // Define o tipo de tabela da tabela corrente
-    public void setTipoTabela(TipoTabela tipo){
-        tipoTab = tipo;
-    }
+	// Define o tipo de tabela da tabela corrente
+	public void setTipoTabela(TipoTabela tipo) {
+		tipoTab = tipo;
+	}
 
-    // Calcula a reducao de palavra usando a tabela corrente
-    public int reducaoPalavra(String palavra){
-        char letras[] = palavra.toCharArray();
-        int r = 0;
-        for(char l: letras) {
-        	r += decodChar(l);
-        }
-        return r;
-    }
+	// Calcula a reducao de palavra usando a tabela corrente
+	public int reducaoPalavra(String palavra) {
+		char letras[] = palavra.toCharArray();
+		int r = 0;
+		for (char l : letras) {
+			r += decodChar(l);
+		}
+		return r;
+	}
 
-    // Calcula a reducao de frase usando a tabela corrente
-    public int reducaoFrase(String frase){
-        String palavras[] = frase.split(" ");
-        int r = 0;
-        for(String p: palavras) {
-        	r += reducaoPalavra(p);
-        }
-        return r;
-    }
+	// Calcula a reducao de frase usando a tabela corrente
+	public int reducaoFrase(String frase) {
+		String palavras[] = frase.split(" ");
+		int r = 0;
+		for (String p : palavras) {
+			r += reducaoPalavra(p);
+		}
+		return r;
+	}
 
-    // Calcula a reducao de uma data no formato dd/mm/aaaa
-    // Se estiver fora do formato lanca IllegalArgumentException
-    public int reducaoData(String data){
-    	
-    	// Incompleto
-    	
-        try {
-        	SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
-        	Date parsedData = formato.parse(data);
-        	if(parsedData.equals(formato)) {
-        		int r = 0;
-	        	String partes[] = data.split("/");
-	        	int dia = Integer.parseInt(partes[0]);
-	        	int mes = Integer.parseInt(partes[1]);
-	        	int ano = Integer.parseInt(partes[2]);
-        	}
-        } catch (IllegalArgumentException | ParseException e) {
-            System.out.println(e);
-        }
-        return 0;
-    }
+	// Calcula a reducao de uma data no formato dd/mm/aaaa
+	// Se estiver fora do formato lanca IllegalArgumentException
+	public int reducaoData(String data) {
+		SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
+		int r = 0;
+		try {
+			formato.parse(data.trim());
+			String dia = String.join("/", data);
+			// incompleto
+		} catch (IllegalArgumentException | ParseException e) {
+			throw new IllegalArgumentException(e + "Formato inválido para data.");
+		}
+		return r;
+	}
 }
---
+++ src/test/java/com/bcopstein/Numerologia/TestRedutor.java
@@ -0,0 +1,65 @@
+package com.bcopstein.Numerologia;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestRedutor {
+	private TipoTabela tipoTab;
+	private Redutor r = new Redutor();
+
+	@Test
+	public void testReducaoPalavraPitagorica() {
+		r.setTipoTabela(TipoTabela.PITAGORICA);
+		String palavra = "PALAVRA"; // 7 + 1 + 3 + 1 + 4 + 9 + 1 = 26
+		r.reducaoPalavra(palavra);
+		assertEquals(26, r.reducaoPalavra(palavra));
+	}
+
+	@Test
+	public void testReducaoFrasePitagorica() {
+		r.setTipoTabela(TipoTabela.PITAGORICA);
+		String frase = "UMA FRASE CURTA Z"; // 3 + 4 + 1 + 6 + 9 + 1 + 1 + 5 + 3 + 3 + 9 + 2 + 1 + 8 = 56
+		r.reducaoFrase(frase);
+		assertEquals(56, r.reducaoFrase(frase));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testPitagoricaThrow() {
+		r.setTipoTabela(TipoTabela.PITAGORICA);
+		r.reducaoPalavra("a");
+	}
+
+	@Test
+	public void testReducaoPalavraChaldean() {
+		r.setTipoTabela(TipoTabela.CHALDEAN);
+		String palavra = "PALAVRA"; // 8 + 1 + 3 + 1 + 6 + 2 + 1 = 22
+		r.reducaoPalavra(palavra);
+		assertEquals(22, r.reducaoPalavra(palavra));
+	}
+
+	@Test
+	public void testReducaoFraseChaldean() {
+		r.setTipoTabela(TipoTabela.CHALDEAN);
+		String frase = "UMA FRASE CURTA Z"; // 6 + 4 + 1 + 8 + 2 + 1 + 3 + 5 + 3 + 6 + 2 + 4 + 1 + 7 = 53
+		r.reducaoFrase(frase);
+		assertEquals(53, r.reducaoFrase(frase));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testChaldeanThrow() {
+		r.setTipoTabela(TipoTabela.CHALDEAN);
+		r.reducaoPalavra("a");
+	}
+	
+	@Test
+	public void testReducaoData() {
+		String data = "10/10/1010";
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testReducaoDataThrow() {
+		String data = "10101010";
+		r.reducaoData(data);
+		assertEquals(22, r.reducaoData(data));
+	}
+}
